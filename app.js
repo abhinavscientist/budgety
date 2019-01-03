c44d6d7a51d35be2e3ad11436476c219fe98c6a1
abhinav agrawal <abhinavscientist@gmail.com>
2019-01-03 17:03:04 +0530

    initial variable and object definition done

---
+++ app.js
@@ -0,0 +1,20 @@
+var incomeTotal, expenseTotal;
+
+var incomeArray = [];
+var expenseArray = [];
+
+var incomeObject = function(name, amount){
+	this.name = name;
+	this.amount = amount;
+};
+
+var expenseObject = function(){
+	this.name = name;
+	this.amount = amount;
+};
+
+//this will go in event listerner
+incomeArray.push(new incomeObject('website', 2500));
+
+//this will go in event listerner
+expenseArray.push(new expenseObject('milk', 200));
