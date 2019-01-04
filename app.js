151e77a94a334aa03894ac1dbdde7e5fcde24974
abhinav agrawal <abhinavscientist@gmail.com>
2019-01-04 11:33:52 +0530

    updateAvailableBudjet done

--- app.js
+++ app.js
@@ -16,9 +16,19 @@ var expenseObject = function(name, amount){
 	this.name = name;
 	this.amount = amount;
 	this.updateIncome = function(){
-		incomeTotal -= amount;
+		expenseTotal += amount;
 	}
 };
+
+function updateAvailableBudget(){
+	var s;
+	if ((incomeTotal - expenseTotal) >= 0){
+		s = '+';
+	} else {
+		s = '';
+	}
+	document.querySelector('.budget__value').textContent = s + ' ' + (incomeTotal - expenseTotal);	
+}
 
 //this will go in event listerner
 document.querySelector('.add__btn').addEventListener('click', function(){
@@ -29,11 +39,15 @@ document.querySelector('.add__btn').addEventListener('click', function(){
 		addIncomeObject = new incomeObject(description, value);
 		incomeArray.push(addIncomeObject);
 		addIncomeObject.updateIncome();
+		document.querySelector('.budget__income--value').textContent = '+ ' + incomeTotal; 
 	} else {	
 		addExpenseObject = new expenseObject(description, value); 
 		expenseArray.push(addExpenseObject);		
 		addExpenseObject.updateIncome();
+		document.querySelector('.budget__expenses--value').textContent = '+ ' + expenseTotal;
+		document.querySelector('.budget__expenses--percentage').textContent = parseInt((expenseTotal/incomeTotal)*100) + '%';
 	}
+	updateAvailableBudget();
 	console.log(incomeArray);
 	console.log(expenseArray);
 	console.log(incomeTotal);
