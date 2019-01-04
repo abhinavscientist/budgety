35d85f7d5f708c70e46caa50517a5c8358b1cb80
abhinav agrawal <abhinavscientist@gmail.com>
2019-01-04 11:05:47 +0530

    adding expenseObject and income object using UI

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var incomeTotal, expenseTotal;
+var incomeTotal = 0;
+var expenseTotal = 0;
 
 var incomeArray = [];
 var expenseArray = [];
@@ -6,15 +7,36 @@ var expenseArray = [];
 var incomeObject = function(name, amount){
 	this.name = name;
 	this.amount = amount;
+	this.updateIncome = function(){
+		incomeTotal += amount; 
+	};
 };
 
-var expenseObject = function(){
+var expenseObject = function(name, amount){
 	this.name = name;
 	this.amount = amount;
+	this.updateIncome = function(){
+		incomeTotal -= amount;
+	}
 };
 
 //this will go in event listerner
-incomeArray.push(new incomeObject('website', 2500));
+document.querySelector('.add__btn').addEventListener('click', function(){
+	var isIncrease = document.querySelector('.add__type').value === 'inc';
+	var description = document.querySelector('.add__description').value;
+	var value = parseInt(document.querySelector('.add__value').value);
+	if ( isIncrease ){
+		addIncomeObject = new incomeObject(description, value);
+		incomeArray.push(addIncomeObject);
+		addIncomeObject.updateIncome();
+	} else {	
+		addExpenseObject = new expenseObject(description, value); 
+		expenseArray.push(addExpenseObject);		
+		addExpenseObject.updateIncome();
+	}
+	console.log(incomeArray);
+	console.log(expenseArray);
+	console.log(incomeTotal);
+});
 
-//this will go in event listerner
-expenseArray.push(new expenseObject('milk', 200));
+
